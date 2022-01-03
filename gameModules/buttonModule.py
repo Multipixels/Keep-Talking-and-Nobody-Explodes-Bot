8875a4e530c514d0a62aff3259fcce5638ac4fd2
Multipixels <ric.motorgeanu@gmail.com>
2022-01-02 19:26:32 -0500

    Made moduleManager modular.

---
+++ gameModules/buttonModule.py
@@ -0,0 +1,11 @@
+from module import Module
+
+class ButtonModule(Module):
+    def __init__(self, serial=""):
+        pass
+
+    def logic(self):
+        pass
+
+    def solve(self, rawInput):
+        pass
--- moduleManager.py
+++ moduleManager.py
@@ -1,5 +1,24 @@
-from gameModules import *
-from gameModules.wiresModule import WiresModule
+import os
+import sys
+from importlib import import_module
+import gameModules
+# from gameModules.wiresModule import WiresModule
+# from gameModules.buttonModule import ButtonModule
+
+#moduleList = []
+for file in os.listdir(f"gameModules"):
+    if file.endswith(".py") and file != '__init__.py':
+        moduleName = file.split(".py")[0]
+        import_module(f"gameModules.{moduleName}")
+        #moduleList.append(moduleName)
+
+#importlib.import_module(f"gameModules\{file}")
+#import_module("gameModules.wiresModule")
+
+#m = dir()
+#print(m)
+#print(moduleList)
+#pkg = importlib.import_module('gameModules')
 
 numbers = {
     'one': '1',
@@ -13,20 +32,45 @@ numbers = {
     'seven': '7',
     'eight': '8',
     'nine': '9',
+    'ten': '10',
     'zero' : '0'
 }
 
+modules = {
+    #'complicated wires': ComplicatedWires(),
+    #'simple wires': WiresModule,
+    'wires': gameModules.wiresModule.WiresModule,
+    'wire': gameModules.wiresModule.WiresModule,
+    'simple button': gameModules.buttonModule.ButtonModule,
+    'button': gameModules.buttonModule.ButtonModule
+}
+
 serialNumber = ""
+numberOfBatteries = 0
 
 def redirectInformation(input):
     if "serial" in input or "cereal" in input:
         return setSerial(input)
-    elif "wire" in input:
-        print(serialNumber)
-        module = WiresModule(serialNumber)
-        return module.solve(input)
+    elif "battery" in input:
+        return setBatteries(input)
     else:
+        for key in modules:
+            if key in input:
+                workModule = modules[key](serialNumber)
+                return workModule.solve(input)
         return [-1, "Invalid input, please try again"]
+        #res = [val for key, val in modules.items() if input in key]
+        #if input in modules:
+        #    print("it works")
+        #else:
+        #    print("no work")
+        #print(res)
+        #module = WiresModule(serialNumber)
+        #return module.solve(input)
+    #else:
+        #return [-1, "Invalid input, please try again"]
+
+#redirectInformation("wires red red red")
 
 def setSerial(input):
     global serialNumber
@@ -51,3 +95,20 @@ def setSerial(input):
     
     speechOutput = f"Serial Number is {tempSerial}"
     return [0, speechOutput]
+
+def setBatteries(input):
+    global numberOfBatteries
+
+    numberOfBatteries = -1
+    speechOutput = ""
+
+    modifiedInput = input.split()
+
+    for word in modifiedInput:
+        if word in numbers:
+            numberOfBatteries = numbers[word]
+            speechOutput = f"There are {numberOfBatteries} batteries."
+            return [0, speechOutput]
+
+    speechOutput = f"Invalid input, please try again."
+    return [-1, speechOutput]
